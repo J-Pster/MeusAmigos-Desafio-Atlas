1ff111d9d74e9e6d91c99d3504fa036f29e9b4f9
João Pster <joaopsterdev@gmail.com>
2022-12-09 15:24:31 -0300

    feat(assets): Adicionado todos os assets do projeto

---
+++ frontend/src/assets/images/index.ts
@@ -0,0 +1,11 @@
+import Logo from "./Logo.png";
+import LogoSvg from "./Logo.svg";
+import Profile from "./Profile.jpg";
+
+const Images = {
+  Logo,
+  LogoSvg,
+  Profile,
+};
+
+export default Images;
