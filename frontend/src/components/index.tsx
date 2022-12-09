f66b1b2005852c580765c1aa7fb4988491322950
João Pster <joaopsterdev@gmail.com>
2022-12-09 15:27:36 -0300

    feat(components): Deletado antigos componentes e alterado o index.tsx

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -1,6 +1,9 @@
-export * from "./section/home/index";
-export * from "./section/about/index";
-export * from "./common/alert/index";
-export * from "./common/rating/index";
-export * from "./common/card/index";
-export * from "./common/item/index";
+// Sections
+export { default as Home } from './section/Home/Home';
+export { default as Login } from './section/Login/Login';
+
+// Common
+export { default as Loading } from './common/Loading/Loading';
+export { default as Header } from './common/Header/Header';
+export { default as Footer } from './common/Footer/Footer';
+export { default as FriendCard } from './common/FriendCard/FriendCard'
