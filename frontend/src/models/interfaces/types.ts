4ccb4edf4c0e8cd9a2104cbff34e30c9f1d80727
João Pster <joaopsterdev@gmail.com>
2022-12-09 15:31:25 -0300

    feat(interfaces): Adicionadas novas interfaces usadas no projeto

--- frontend/src/models/interfaces/types.ts
+++ frontend/src/models/interfaces/types.ts
@@ -3,6 +3,8 @@ export interface BasicRatingProps {
     text: string
 }
 
+// Example
+
 export interface ExampleResponse {
     id: number;
     uid: string;
@@ -11,6 +13,44 @@ export interface ExampleResponse {
     bank_name: string;
     routing_number: string;
     swift_bic: string;
+}
+
+// Login
+
+export interface LoginResponse {
+    token: string;
+    success: boolean;
+}
+
+export interface LoginRequest {
+    email: string;
+    password: string;
+}
+
+// List Friends
+
+export interface ListFriendsResponse {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: {
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: string;
+        geo: {
+            lat: string;
+            lng: string;
+        };
+    };
+    phone: string;
+    website: string;
+    company: {
+        name: string;
+        catchPhrase: string;
+        bs: string;
+    };
 }
 
 export interface MediaCardProps {
