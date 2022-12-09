85abd3ab1ea1e846a97c2ccc5b8c5674fb056bd8
João Pster <joaopsterdev@gmail.com>
2022-12-09 15:28:22 -0300

    feat(hooks): Implementado o Hook de estado persistente

---
+++ frontend/src/hooks/usePersistentState.ts
@@ -0,0 +1,25 @@
+/* eslint-disable consistent-return */
+import { useEffect, useState } from "react";
+
+export default function usePersistedState(key: any, initialState: any) {
+  const [state, setState] = useState(() => {
+    const storage = localStorage.getItem(key);
+
+    if (storage) {
+      return JSON.parse(storage);
+    }
+    return initialState;
+  });
+
+  useEffect(() => {
+    try {
+      if (typeof state === "undefined") return localStorage.removeItem(key);
+
+      localStorage.setItem(key, JSON.stringify(state));
+    } catch (error) {
+      localStorage.setItem(key, JSON.stringify(initialState));
+    }
+  }, [key, state, initialState]);
+
+  return [state, setState];
+}
