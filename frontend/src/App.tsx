0e6e1e3c68f4cac8946c4991e4c52edf06617a00
João Pster <joaopsterdev@gmail.com>
2022-12-18 15:42:06 -0300

    feat(test): test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { Container } from "@mui/material";
 import { Home, Footer, Header } from "components";
 import React from "react";
 
+// Test SSH
+
 const App: React.FC = (): React.ReactElement => {
   return (
     <Home />
